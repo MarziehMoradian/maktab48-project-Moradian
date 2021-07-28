c9d8c90c1d60c2d9e67b6689f7fc616170cb3c30
m.moradian <marziehmoradiann@gmail.com>
2021-07-28 10:58:48 +0430

    Add DatePicker

--- src/components/mainPageComponents/CheckOut/AddressForm.js
+++ src/components/mainPageComponents/CheckOut/AddressForm.js
@@ -1,25 +1,45 @@
 import React,{useState} from 'react'
-import { InputLable,Select, MenuItem, Button, Grid, Typography } from '@material-ui/core'
+import { Select, MenuItem, Button, Grid, Typography, Input, TextField, InputLabel, Divider } from '@material-ui/core'
 import { useForm, FormProvider } from 'react-hook-form';
 import CustomTextField from './CustomTextField';
 import { Link } from 'react-router-dom';
+import {DatePicker, DateTimePicker, DateRangePicker, DateTimeRangePicker} from "react-advance-jalaali-datepicker";
 const AddressForm = ({next}) => {
     const method = useForm()
-    const [shippingCountries,SetShippingCountries] = useState([]);
-    const [shippingCountry,SetShippingCountry] = useState();
-   
+  
+  const  DatePickerInput= (props) =>  {
+        return (
+           <Grid item xs={12} sm={12} >
+            <TextField   {...props}  fullWidth size="small" />
+            </Grid>
+        )
+    }
     return (
         <div>
-            <Typography variant="h6" gutterBottom>Shipping Adress</Typography>
+            {/* <Typography variant="h6" gutterBottom>Shipping Adress</Typography> */}
             <FormProvider {...method}>
                 <form onSubmit={method.handleSubmit((data)=> next({...data}))}>
                     <Grid container spacing={3}>
-                        <CustomTextField required name="firstName" lable="First Name" />
-                        <CustomTextField required name="LastName" lable="Last Name" />
-                        <CustomTextField required name="address" lable="Adress" />
-                        <CustomTextField required name="phoneNumber" lable="Phone Number" />
-                        <CustomTextField required name="Date" lable="Reciev Date" />
-                        <CustomTextField  name="email" lable="Email" />
+                        <InputLabel>نام:</InputLabel>
+                        <CustomTextField required name="firstName" />
+                        <InputLabel>نام خانوادگی:</InputLabel>
+                        <CustomTextField required name="LastName"  />
+                        <InputLabel>آدرس:</InputLabel>
+                        <CustomTextField required name="address"  />
+                        <InputLabel>شماره تلفن:</InputLabel>
+                        <CustomTextField required name="phoneNumber" />
+                        <br/>
+                        <InputLabel>  تاریخ تحویل محصول یا محصولات خود را مشخص کنید:  </InputLabel>
+                        
+                        <DatePicker
+                        inputComponent={DatePickerInput}
+                        placeholder="انتخاب تاریخ"
+                        format="jYYYY/jMM/jDD"
+                        // onChange={this.change}
+                        id="datePicker"
+                        preSelected="1400/05/15"
+                        />
+                        {/* <CustomTextField  name="email" lable="Email" /> */}
                         {/* <Grid item xs={12} sm={6}>
                             <InputLable>Shipping country</InputLable>
                             <Select  value ={shippingCountries} fullWidth onChange={}>
@@ -29,6 +49,8 @@ const AddressForm = ({next}) => {
                         </Grid> */}
                         
                     </Grid>
+                    <br/>
+                    <br/>
                     <br/>
                     <div style={{display:'flex' , justifyContent:'space-between'}}>
                         <Button variant="outlined" component={Link} to ="/basket">Back to go</Button>
--- src/components/mainPageComponents/CheckOut/CustomTextField.js
+++ src/components/mainPageComponents/CheckOut/CustomTextField.js
@@ -3,10 +3,15 @@ import { TextField,Grid } from '@material-ui/core';
 import { useFormContext,Controller } from 'react-hook-form';
 const CustomTextField = ({name,lable,required}) => {
     const {control} = useFormContext()
+    const input = (props) => {
+        return(
+            <TextField variant="outlined" fullWidth size="small" />
+        )
+    }
     return (
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={12}>
             <Controller
-                as={TextField}
+                as={input}
                 control={control}
                 fullWidth
                 name={name}
