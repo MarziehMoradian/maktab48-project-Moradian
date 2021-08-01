2c45f0d6f5136b2b36aa67f82f38135d7599c090
m.moradian <marziehmoradiann@gmail.com>
2021-08-01 16:20:08 +0430

    add useLocation in productscomponent

--- src/components/mainPageComponents/products/ProductDetail.js
+++ src/components/mainPageComponents/products/ProductDetail.js
@@ -57,7 +57,7 @@ const ProductDetail = ({onClick}) => {
             <Typography variant="body1" className="desc">{selectedProduct.description}</Typography>
               <div className="row">
                 <TextField type="number" size="small" variant="outlined" className="card desc" style={{width:'70px'}} value={value} onChange={(e)=> setValue(e.target.value <= 0 ? 0 :e.target.value ) }/>
-                <Button  variant="contained" color="primary" className="cart" onClick={() => onClick(selectedProduct.id,value[(value.length)-1])}>اضافه کردن به سبد خرید</Button>
+                <Button  variant="contained" style={{backgroundColor:'#0d5b36'}} className="cart" onClick={() => onClick(selectedProduct.id,value[(value.length)-1])}>اضافه کردن به سبد خرید</Button>
             </div>
           </div>
 
--- src/components/mainPageComponents/products/Products.js
+++ src/components/mainPageComponents/products/Products.js
@@ -1,14 +1,13 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import { Grid, Typography} from '@material-ui/core';
-import { connect } from 'react-redux';
 import Product from './product/Product'
 import {useStyles} from './style';
 import { Link } from 'react-router-dom';
-
+import { useLocation } from 'react-router';
 const Products = ({products,onAddToCart,onClick,categoryName,num}) => {
        
         const classes = useStyles()
-
+        const location = useLocation()
    
     return(
     <main className={classes.content}>
@@ -17,9 +16,14 @@ const Products = ({products,onAddToCart,onClick,categoryName,num}) => {
             <Link to={`/product/category/${categoryName}`} style={{textDecorationLine:'none',color:'#000'}}>
                 <Typography variant="h2" className={classes.type}>{categoryName}</Typography>
             </Link>
+            {location.pathname !== `/product/category/${categoryName}` ? (
+
             <Link to={`/product/category/${categoryName}`} style={{textDecorationLine:'none',color:'#000',marginTop:'30px'}}>
                 <Typography variant="h6" >بیشتر</Typography>
             </Link>
+            ):(
+               null
+            )}
         </div>
         
        
